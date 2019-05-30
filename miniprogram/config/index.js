817939687ae58e9807c0d2b63ebe9eef1481c961
wisard <yongjiandefashi@163.com>
2019-05-30 10:56:39 +0800

    production config
    正式环境配置调整

--- miniprogram/config/index.js
+++ miniprogram/config/index.js
@@ -1,2 +1,3 @@
 // export const apiDomain = 'http://localhost:9500'
-export const apiDomain = 'http://192.168.21.91:9500'
+// export const apiDomain = 'http://192.168.21.91:9500'
+export const apiDomain = 'https://www.yincangjia.net'
